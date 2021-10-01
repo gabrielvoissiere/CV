f046d6842aaf376812b1a6f3f50f0b080b503fca
gabrielvoissiere <gabvoissiere.pro@yahoo.com>
2021-10-01 13:47:17 +0200

    md

--- deploy.sh
+++ deploy.sh
@@ -7,19 +7,6 @@ set -e
 npm run build
 
 # navigate into the build output directory
-cd dist
-
-# if you are deploying to a custom domain
-# echo 'www.example.com' > CNAME
-
-git init
-git add -A
+git add dist
 git commit -m 'deploy'
-
-# if you are deploying to https://<USERNAME>.github.io
-# git push -f git@github.com:<USERNAME>/<USERNAME>.github.io.git main
-
-# if you are deploying to https://<USERNAME>.github.io/<REPO>
-git push -f https://github.com/gabrielvoissiere/CV.git main:gh-pages
-
-cd -
+git subtree push --prefix dist origin gh-pages
